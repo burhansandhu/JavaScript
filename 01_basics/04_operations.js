c8d233320c9ca9b9b0c45d4742efe4911772dc30
Burhan ul  haq <burhanulhaq60@gmail.com>
2024-11-03 15:43:02 +0000

    simple operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,12 @@
+let str1 = "Burhan"
+let str2 =  "Sandhu"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
