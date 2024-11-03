edd8fb0cdf6675fa5eaf57bbb05c09edbc42525f
Burhan ul  haq <burhanulhaq60@gmail.com>
2024-11-03 13:10:11 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,7 @@
+let name = "Burhan"
+let age = 21
+let salary = undefined
+let a = null
+let isEmployee = false
+
+console.table([typeof name, typeof age, typeof salary, typeof a, typeof isEmployee])
