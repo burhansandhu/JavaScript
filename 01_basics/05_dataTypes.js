3059c8b53c9d1ca7ddfe021c2b28122954df108f
Burhan ul  haq <burhanulhaq60@gmail.com>
2024-11-04 16:26:30 +0000

    data types

---
+++ 01_basics/05_dataTypes.js
@@ -0,0 +1,21 @@
+/* primitive Data Types:
+
+String, Number, Boolean, BigInt, null, undefined, symbol
+
+Non-Premitive (Reference):
+
+Array, Objects, Functions
+*/
+
+const my_array = ["Burhan", "Riaz", "Tayyab"]
+
+let my_obj = {
+    name:"Burhan",
+    age: 21,
+    University: "UMT"
+}
+
+const my_function = Function() 
+{
+    console.log("Hello World"); 
+}
