3fe6e5e709d876e9590e46176679b5d14c35d394
Burhan ul  haq <burhanulhaq60@gmail.com>
2024-11-05 17:04:57 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const name = "Burhan"
+const age = 21
+
+console.log(name + " " + age);
+
+console.log(`My name is ${name} and my age is ${age}`); //work as f-string in python
+
+const college = new String("UMT") //this is another way to declare a string by using this string store with index as in array and we can access any index value of string
+console.log(college);
+
+console.log(college[1]); //output = 'M' because M is at index 1 of college
+
+console.log(college.toLowerCase()); //we access to multiple functions of string using . with variable name
+
