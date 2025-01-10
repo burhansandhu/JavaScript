2905c6a06b327408610e2e1b66688baac33660c3
Burhan ul  haq <burhanulhaq60@gmail.com>
2025-01-10 17:35:24 +0000

    learning arrays

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(typeof myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+
+let newDate = new Date(2003, 11, 18)
+console.log(newDate.toDateString());
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+const myArr = [3,5,7,"Ali"] //just like list in python
+//console.log(myArr);
+//console.log(myArr[2])
+
+//myArr.push(10)
+//console.log(myArr);
+
+//myArr.unshift(9) //add value in start of array
+//console.log(myArr);
+
+//myArr.shift() // remove value from start of array
+//console.log(myArr);
+
+console.log(myArr.includes("Ali")); //return true if value exist false otherwise
+
+console.log(myArr.indexOf(5)); //return -1 if value not exist in array
