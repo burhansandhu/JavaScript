11d8c4df5de3f5b14749613dc3492f842c2282a2
Burhan ul  haq <burhanulhaq60@gmail.com>
2026-01-30 06:33:02 +0000

    Data Types

--- 01_basics/05_dataTypes.js
+++ 01_basics/05_dataTypes.js
@@ -7,14 +7,17 @@ Non-Premitive (Reference):
 Array, Objects, Functions
 */
 
+// Array
 const my_array = ["Burhan", "Riaz", "Tayyab"]
 
+// Object
 let my_obj = {
     name:"Burhan",
     age: 21,
     University: "UMT"
 }
 
+//  Function
 const my_function = Function() 
 {
     console.log("Hello World"); 
